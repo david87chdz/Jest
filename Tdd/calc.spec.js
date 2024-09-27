5b98d5d08660374ba8a99604fbbea6de471d208e
david87chdz <david87chdz@gmail.com>
2024-09-27 10:44:37 +0200

    Continue the course

---
+++ Tdd/calc.spec.js
@@ -0,0 +1,18 @@
+it.todo('should add two numbres', () => {
+    //Arange
+
+    //Act
+
+    //Assert
+});
+
+it('should multiply two numbers', () => {
+    
+});
+it('should ', () => {
+    //Arange
+
+    //Act
+
+    //Assert
+});
